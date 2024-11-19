b907f2194a4c1a105e69589c778db214ed4ca19d
digvijaykarande <karandedigvijay2@gmail.com>
2024-11-19 21:20:30 +0530

    movieshouse2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import { Pageone } from './Pageone'
-import { NavLink, Route, Router, Routes } from 'react-router-dom'
-import MoviesPage from './MoviesPage'
-import ShowsPage from './ShowsPage'
-import NavBar from './NavBar'
-import { SearchResult } from './SearchResult'
-import Detials from './Detials'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MoviesPage from './MoviesPage';
+import ShowsPage from './ShowsPage';
+import NavBar from './NavBar';
+import SearchResult from './SearchResult';
+import Detials from './Detials';
+import Pageone from './Pageone';
 
 export default function App() {
   return (
-    
-    <>
-    
+    <Router>
       <NavBar />
       <Routes>
-      <Route path='/movies'element={<MoviesPage/>} />
-      <Route path='/shows'element={<ShowsPage/>} />
-      <Route path='/search'element={<SearchResult/>} />
-      <Route path='/'element={<Pageone/>} />
-      <Route path='/detail/:movieId'element={<Detials/>} />
+        <Route path='/movies' element={<MoviesPage />} />
+        <Route path='/shows' element={<ShowsPage />} />
+        <Route path='/search' element={<SearchResult />} />
+        <Route path='/' element={<Pageone />} />
+        <Route path='/detail/:movieId' element={<Detials />} />
       </Routes>
-   
-  
-   </>
-  )
+    </Router>
+  );
 }
